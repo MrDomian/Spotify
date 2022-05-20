1c7d8838a2efb4de06e886024d6fab969b0cb63e
MrDomian <41548647+MrDomian@users.noreply.github.com>
2022-05-20 11:19:51 +0200

    Add Title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { getSession, GetSessionParams } from "next-auth/react";
+import Head from 'next/head'
 import Center from "../components/Center";
 import Sidebar from "../components/Sidebar";
 import Player from "../components/Player";
@@ -6,10 +7,13 @@ import Player from "../components/Player";
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
+      <Head>
+        <title>Spotify4Fun</title>
+      </Head>
+
       <main className="flex sticky">
         <Sidebar />
         <Center />
-        
       </main>
 
       <div className='sticky bottom-0'>
